17cf50db7e3a319054f0d97e34c0c5cc517a8f6b
Kevin Mejia <kwmejia9@gmail.com>
2024-05-06 08:48:52 -0500

    API BEAUTY SALON

--- _06_Api_Salon_Belleza/src/main/java/com/riwi/beautySalon/domain/entities/ClientEntity.java
+++ _06_Api_Salon_Belleza/src/main/java/com/riwi/beautySalon/domain/entities/ClientEntity.java
@@ -1,11 +1,7 @@
 package com.riwi.beautySalon.domain.entities;
 
-import com.riwi.beautySalon.utils.enums.RoleEmployee;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -14,12 +10,12 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Entity( name = "employee")
+@Entity(name = "client")
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-public class Employee {
+public class ClientEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -27,11 +23,8 @@ public class Employee {
     private String firstName;
     @Column(length = 100, nullable = false)
     private String lastName;
-    @Column(length = 100)
-    private String email;
     @Column(length = 20)
     private String phone;
-    @Column(nullable = false)
-    @Enumerated(EnumType.STRING)
-    private RoleEmployee role;
+    @Column(length = 100)
+    private String email;
 }
