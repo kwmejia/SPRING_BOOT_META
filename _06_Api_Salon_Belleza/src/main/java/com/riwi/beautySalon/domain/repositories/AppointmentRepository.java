58b39e5f7e4cd10cb776790bc3744db8577f0b41
Kevin Mejia <kwmejia9@gmail.com>
2024-05-07 08:59:54 -0500

    dto

--- _06_Api_Salon_Belleza/src/main/java/com/riwi/beautySalon/domain/repositories/AppointmentRepository.java
+++ _06_Api_Salon_Belleza/src/main/java/com/riwi/beautySalon/domain/repositories/AppointmentRepository.java
@@ -1,6 +1,9 @@
 package com.riwi.beautySalon.domain.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.riwi.beautySalon.domain.entities.Appointment;
@@ -8,5 +11,8 @@ import com.riwi.beautySalon.domain.entities.Appointment;
 @Repository
 public interface AppointmentRepository
      extends JpaRepository<Appointment, Long> {
-    
+
+        @Query("select p from appointment p join fetch p.client c where c.id = :idClient")
+        public Optional<Appointment> findByClientId(Long idClient);
+
 }
--- _06_Api_Salon_Belleza/src/main/java/com/riwi/beautySalon/domain/repositories/ServiceRepository.java
+++ _06_Api_Salon_Belleza/src/main/java/com/riwi/beautySalon/domain/repositories/ServiceRepository.java
@@ -1,6 +1,10 @@
 package com.riwi.beautySalon.domain.repositories;
 
+import java.math.BigDecimal;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.riwi.beautySalon.domain.entities.ServiceEntity;
@@ -8,5 +12,11 @@ import com.riwi.beautySalon.domain.entities.ServiceEntity;
 @Repository
 public interface ServiceRepository 
     extends JpaRepository<ServiceEntity,Long> {
-    
+        @Query("select s from service s where s.price between :min and :max")
+        public List<ServiceEntity> selectBetweenPrice(BigDecimal min, BigDecimal max);
+
+        // @Query( value = "select * from service where service.name like :search", nativeQuery = true)
+        @Query("select s from service s where s.name like :search")
+        public List<ServiceEntity> findByNameContaining(String search);
+
 }
