ef195da104e1608f37f6742cd53d4126ca407d4d
Kevin Mejia <kwmejia9@gmail.com>
2024-05-10 09:31:23 -0500

    10-05-2024 Entity User

--- _06_Api_Salon_Belleza/src/main/java/com/riwi/beautySalon/api/controllers/AuthController.java
+++ _06_Api_Salon_Belleza/src/main/java/com/riwi/beautySalon/api/controllers/AuthController.java
@@ -1,13 +1,27 @@
 package com.riwi.beautySalon.api.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.riwi.beautySalon.api.dto.request.RegisterReq;
+import com.riwi.beautySalon.api.dto.response.AuthResp;
+import com.riwi.beautySalon.infraestructure.abstract_services.IAuthService;
+
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+
 @RestController
 @RequestMapping
+@RequiredArgsConstructor
 public class AuthController {
     
+    @Autowired
+    private final IAuthService authService;
 
     @PostMapping(path = "/auth/login")
     public String login(){
@@ -15,7 +29,9 @@ public class AuthController {
     }
 
     @PostMapping(path = "/auth/register")
-    public String register(){
-        return "HACIENDO EL LOGIN";
+    public ResponseEntity<AuthResp> register(
+        @Validated @RequestBody RegisterReq request
+    ){
+        return ResponseEntity.ok(this.authService.register(request));
     }
 }
