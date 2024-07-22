06f64585ec3e801be0ff05d0ad028739ed7f5469
Kevin Mejia <kwmejia9@gmail.com>
2024-07-22 12:55:29 -0500

    add service registry

--- _07_Microservicios/clanes-crud/src/main/java/com/riwi/clanes_crud/ClanesCrudApplication.java
+++ _07_Microservicios/clanes-crud/src/main/java/com/riwi/clanes_crud/ClanesCrudApplication.java
@@ -2,8 +2,10 @@ package com.riwi.clanes_crud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient // Para que el servicio se registre en el servidor Eureka
 public class ClanesCrudApplication {
 
 	public static void main(String[] args) {
---
+++ _07_Microservicios/service-registry/src/main/java/com/riwi/service_registry/ServiceRegistryApplication.java
@@ -0,0 +1,15 @@
+package com.riwi.service_registry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer // Enable eureka server
+public class ServiceRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceRegistryApplication.class, args);
+	}
+
+}
